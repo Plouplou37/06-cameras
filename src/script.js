ac53b685447a04a5db4bf3c1330c89123d2eba3b
Plouplou37 <quentin.plourdeau@gmail.com>
2024-01-22 20:39:29 +0800

    end of the lesson on type of cameras

--- src/script.js
+++ src/script.js
@@ -1,4 +1,23 @@
+import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+/**
+ * Cursor
+ */
+// The listener here listen to the event happening on the displayed window. The event type refered to 'mousemove'
+// the function given in parameter determine the behavior of the code when this particular event happen.
+const cursor = {
+    x:0,
+    y:0}
+
+window.addEventListener('mousemove', (event) => {
+
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+    //console.log("x move is ", cursor.x, "y move is ", cursor.y)
+})
+
 
 /**
  * Base
@@ -23,11 +42,23 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height, 0.1, 100)
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// controls.target.y = 2
+// controls.update()
+//const aspectRatio = sizes.width/sizes.height
+// render a little further on the left and on the right for the cube to be well displayed in a rectangle renderer !
+// Need to get the aspecctRatio of the renderer to adapt the cube dimension properly
+//const camera = new THREE.OrthographicCamera(-6 * aspectRatio, 6 * aspectRatio, 6, -6, 0.1, 100)
+// const camera = new THREE.OrthographicCamera(-6 , 6, 6 / aspectRatio, -6 / aspectRatio, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 3
+//console.log(camera.position.distanceTo(mesh.position))
+//camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -44,7 +75,17 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
+    //When applying sin and cos on two axes. Imagine the trigonometry circle with the two axes being x and z. 
+    //camera.position.x =  2*Math.sin(cursor.x * 2 * Math.PI)
+    //Need to add the 2*PI to do a full circle revolution
+    //camera.position.z =  2*Math.cos(cursor.x * 2 * Math.PI) 
+    //camera.position.y = cursor.y * 10
+    
+    //camera.lookAt(mesh.position)
+
+    //update controls. Need to update the control before each frame. 
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
